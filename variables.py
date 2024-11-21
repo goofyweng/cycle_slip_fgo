3d39803d84e3559b02fddb9a810b74e4008ebda9
wengsp <peter072627@gmail.com>
2024-11-21 22:33:05 +0100

    -Add .py files for nonlinear LS estimation. -Add linear toy example and nonlinear toy GNSS example to play with nonlinear_least_squares.nonlinear_least_squares(). -Fixed a potential bug in line 29 in nonlinear_least_squares.py

---
+++ variables.py
@@ -0,0 +1,6 @@
+#criteria for solver to stop
+criteria = 1e-7
+
+#numbers used to check matrices are well conditioned, injective, ...
+absolute_numerical_threshold = 1e-10 #tolerance with respect to 0
+relative_numerical_threshold = 1e-12 #tolerance of smallest/largest singular values
