87c286a2b1d22d1fa7cc02251c85a86bfa62911c
ormiseragon <a21653933@gmail.com>
2024-12-17 19:03:26 +0100

    - extract the data for TDCP use

--- GNSS_example.py
+++ GNSS_example.py
@@ -6,18 +6,47 @@ from nonlinear_least_squares import nonlinear_least_squares
 
 def build_h(data, epoch):
 
-    # Choose the data from the same epoch
-    epoch_data = data[data["time_of_reception_in_receiver_time"] == epoch]
-    
-    if epoch_data.empty:
-        print(f"No data found for epoch {epoch}")
+    # Choose the data from two chosen epoch
+    epoch_data1 = data[data["time_of_reception_in_receiver_time"] == epoch[0]]
+    epoch_data2 = data[data["time_of_reception_in_receiver_time"] == epoch[1]]
+
+    if epoch_data1.empty or epoch_data2.empty:
+        print(f"No data for one of the epochs {epoch}")
         return None
 
-    # Etract data from this epoch
-    visible_sat = epoch_data["prn"].tolist()
-    sat_coor = epoch_data[["sat_pos_x_m", "sat_pos_y_m", "sat_pos_z_m"]].values
-    sat_clock_bias = epoch_data["sat_clock_offset_m"].values
-    c_obs_m = epoch_data["C_obs_m"].values
+    # find the common sats appear in both epochs
+    sat_epoch1 = set(epoch_data1["prn"])
+    sat_epoch2 = set(epoch_data2["prn"])
+    common_sats = sorted(sat_epoch1 & sat_epoch2)  # find intersection and sort
+    
+    if not common_sats:
+        print(f"No common satellites found between epochs {epoch}")
+        return None
+
+    # filter only the data from the common sats
+    epoch_data1 = epoch_data1[epoch_data1["prn"].isin(common_sats)].set_index("prn")
+    epoch_data2 = epoch_data2[epoch_data2["prn"].isin(common_sats)].set_index("prn")
+
+    sat_coor1 = epoch_data1.loc[common_sats, ["sat_pos_x_m", "sat_pos_y_m", "sat_pos_z_m"]].values
+    sat_coor2 = epoch_data2.loc[common_sats, ["sat_pos_x_m", "sat_pos_y_m", "sat_pos_z_m"]].values
+    sat_clock_bias1 = epoch_data1.loc[common_sats, "sat_clock_offset_m"].values
+    sat_clock_bias2 = epoch_data2.loc[common_sats, "sat_clock_offset_m"].values
+    C_obs_m1 = epoch_data1.loc[common_sats, "C_obs_m"].values
+    C_obs_m2 = epoch_data2.loc[common_sats, "C_obs_m"].values
+    L_obs_cycles1 = epoch_data1.loc[common_sats, "L_obs_cycles"].values
+    L_obs_cycles2 = epoch_data2.loc[common_sats, "L_obs_cycles"].values
+
+    # reorder code obs
+    code_obs = np.empty((2 * len(common_sats), 1)) 
+    code_obs[0::2] = C_obs_m1.reshape(-1, 1)  # even row epoch1
+    code_obs[1::2] = C_obs_m2.reshape(-1, 1)  # odd row epoch2
+
+    # reorder cycle obs
+    cycle_obs = np.empty((2 * len(common_sats), 1))  
+    cycle_obs[1::2] = L_obs_cycles2.reshape(-1, 1)
+
+    # upper part is Code Obs from common sats for both epoch, and low part the Cycle Obs
+    h = np.concatenate([code_obs, cycle_obs], axis=0)
 
     # model for toy GNSS example, nonlinear
     def h_toy_GNSS_w_usr_clk_b(x_u, x_s):
@@ -25,9 +54,9 @@ def build_h(data, epoch):
         rho = r + (x_u[-1] - x_s[-1,:]).reshape(-1,1) # add clock bias to build pseudorange
         jacobian = np.hstack((-1 * (x_s[0:3,:] - x_u[0:3]).T / r, np.ones((rho.size,1))))  # each row is a unit vector from user to sat
         return (rho, jacobian)
-    
 
-    return visible_sat, sat_coor, sat_clock_bias, c_obs_m, h_toy_GNSS_w_usr_clk_b
+    return common_sats, sat_coor1, sat_coor2, sat_clock_bias1, sat_clock_bias2, \
+    C_obs_m1, C_obs_m2, L_obs_cycles1, L_obs_cycles2, h_toy_GNSS_w_usr_clk_b
 
 
 if __name__ == "__main__":
@@ -77,8 +106,8 @@ if __name__ == "__main__":
     # calculate the number of epoch
     epoch = data_gps_c1c["time_of_reception_in_receiver_time"].unique() 
     sorted_epochs = sorted(epoch)
-    first_epoch = sorted_epochs[0]
-    visible_sat, sat_coor, sat_clock_bias, c_obs_m, h = build_h(data_gps_c1c, first_epoch)
+    chosen_epochs = [sorted_epochs[0], sorted_epochs[1]]
+    visible_sat, sat_coor, sat_clock_bias, c_obs_m, h = build_h(data_gps_c1c, chosen_epochs)
     R = np.eye(c_obs_m.size)
 
     # satellite states
