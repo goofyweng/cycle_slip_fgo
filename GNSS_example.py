694405279213e22c8667101dda04414889783758
wengsp <peter072627@gmail.com>
2024-12-19 22:21:10 +0100

    -modify build_h function name to build_h_A. -modify h_toy_GNSS_code_TDCP function name to h_GNSS_code_TDCP. -add comments in h_GNSS_code_TDCP function. -check if the chosen epochs contain cycle slip with "LLI_chosen_epochs" -start the LS with a random x0 vector with values normally distributed around 0. -add fault detection to detect cycle slip. -plot the test statistic and threshold on chi-squared pdf.

--- GNSS_example.py
+++ GNSS_example.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 from nonlinear_least_squares import nonlinear_least_squares
 from scipy.linalg import block_diag
 from ecef2lla import ecef2lla
+import scipy.stats as stats
+from plot_dist import plot_non_central_chi2, draw_vertical_line
 
 
-def build_h(data, epoch):
+def build_h_A(data, epoch):
 
     # Choose the data from two chosen epoch
     epoch_data1 = data[data["time_of_reception_in_receiver_time"] == epoch[0]]
@@ -62,7 +64,7 @@ def build_h(data, epoch):
     y = np.concatenate([code_obs, cycle_obs], axis=0)
 
     # model for GNSS code and TDCP, nonlinear
-    def h_toy_GNSS_code_TDCP(x_u, x_s1, x_s2):
+    def h_GNSS_code_TDCP(x_u, x_s1, x_s2):
         # x_u1 is the user state at epoch 1, shape (4x1), [x1,y1,z1,tb1]^T
         # x_u2 is the user state at epoch 2, shape (4x1)
         x_u1 = x_u[0:4]
@@ -86,15 +88,18 @@ def build_h(data, epoch):
         # assume phase measurement has the same model as pseudorange
         phase = rho
         # concate rho and phase array vertically
-        h_x = np.vstack((rho, phase))
+        h_x = np.vstack((rho, phase)) # shape should be (4kx1)
 
         # build jacobian
+        # jacobian for pseudorange for 1st epoch
         jacobian_rho_1 = np.hstack(
             (-1 * (x_s1[0:3, :] - x_u1[0:3]).T / r_1, np.ones((r_1.size, 1)))
         )
+        # jacobian for pseudorange for 2nd epoch
         jacobian_rho_2 = np.hstack(
             (-1 * (x_s2[0:3, :] - x_u2[0:3]).T / r_2, np.ones((r_2.size, 1)))
         )
+        # create the space to store jaboian matrix for 1st and 2nd epoch
         jacobian_rho = np.zeros(
             (
                 jacobian_rho_1.shape[0] + jacobian_rho_2.shape[0],
@@ -102,10 +107,12 @@ def build_h(data, epoch):
             ),
             dtype=jacobian_rho_1.dtype,
         )
+        # assign jacobian_rho_1 to even rows, 1st to 4th columns
         jacobian_rho[0::2, 0:4] = jacobian_rho_1
+        # assign jacobian_rho_2 to add rows, 5th to last columns
         jacobian_rho[1::2, 4:] = jacobian_rho_2
         # assume phase measurement has the same model as pseudorange
-        jacobian = np.vstack((jacobian_rho, jacobian_rho))
+        jacobian = np.vstack((jacobian_rho, jacobian_rho)) # shape should be (4kx8)
 
         return (h_x, jacobian)
 
@@ -118,9 +125,9 @@ def build_h(data, epoch):
 
     # build A matrix
     A = block_diag(np.eye(2 * num_sats), D)
-
+    # build TDCP model with A matrix
     def h_A(x_u, x_s1, x_s2):
-        hx, jacobian = h_toy_GNSS_code_TDCP(x_u=x_u, x_s1=x_s1, x_s2=x_s2)
+        hx, jacobian = h_GNSS_code_TDCP(x_u=x_u, x_s1=x_s1, x_s2=x_s2)
         return (A @ hx, A @ jacobian)
 
     return (
@@ -180,25 +187,42 @@ if __name__ == "__main__":
     # plt.legend(data_gps_c1c.groupby("prn").groups.keys())
 
     # count numbers of detected cycle slips
-    print(f" numbers of detected cycle slips:  {(data_gps_c1c.LLI == 1).sum()}")
+    print(f"numbers of detected cycle slips:  {(data_gps_c1c.LLI == 1).sum()}")
 
     # calculate the number of epoch
     epoch = data_gps_c1c["time_of_reception_in_receiver_time"].unique()
     sorted_epochs = sorted(epoch)
     chosen_epochs = [sorted_epochs[0], sorted_epochs[1]]
+    print(f"Selected epochs {chosen_epochs}")
+
+    # check if we have LLI == 1, i.e. cycle slip, in the selected epochs
+    LLI_chosen_epochs = data_gps_c1c[
+        (data_gps_c1c["time_of_reception_in_receiver_time"].isin(chosen_epochs))
+    ].LLI.to_numpy()
+    fault_true = np.any(LLI_chosen_epochs)
+    print(f"Cycle clip in selected epochs? => {fault_true}")
+
+    # get the satellite coordinates and clock bias at 1st and 2nd epochs
+
     (
-        common_sats,
-        sat_coor1,
-        sat_coor2,
-        sat_clock_bias1,
-        sat_clock_bias2,
-        y,
-        A,
-        h_A,
-    ) = build_h(data_gps_c1c, chosen_epochs)
+        common_sats,  # satellite prns exist in both 1st and 2nd epoch
+        sat_coor1,  # satellite coordinate at 1st epoch
+        sat_coor2,  # satellite coordinate at 2nd epoch
+        sat_clock_bias1,  # satellite clock bias at 1st epoch
+        sat_clock_bias2,  # satellite clock bias at 2nd epoch
+        y,  # obseravtion vector
+        A,  # A matrix used to construch TDCP
+        h_A,  # (A @ h) function
+    ) = build_h_A(data_gps_c1c, chosen_epochs)
+    
+    #################
+    # y[12,0] += 20 # manually add cycle slip, hopfully we can detect it
+    #################
+
     # covariance matrix
     sigma_code = 3
     sigma_phase = 1
+    # factor uncerntainty model
     R = block_diag(
         sigma_code**2 * np.eye(2 * sat_coor1.shape[0]),
         sigma_phase**2 * np.eye(2 * sat_coor1.shape[0]),
@@ -208,11 +232,15 @@ if __name__ == "__main__":
     x_s1 = np.vstack((sat_coor1.T, sat_clock_bias1))
     # satellite states at second epoch
     x_s2 = np.vstack((sat_coor2.T, sat_clock_bias2))
-    x0 = np.array([0, 0, 0, 0, 0, 0, 0, 0]).reshape(-1, 1)
+
+    np.random.seed(42)  # Set a seed for reproducibility
+    x0 = np.random.randn(8, 1)  # normally distributed random init x0 around 0
     # LS
+    print("LS start...")
     estimate_result = nonlinear_least_squares(
         h_A, A @ y, A @ R @ A.T, x0, x_s1=x_s1, x_s2=x_s2
     )
+    # convert estimate position from ECEF to LLA
     estimate_lla_epoch1 = np.array(
         ecef2lla(estimate_result[0], estimate_result[1], estimate_result[2])
     ).reshape(-1)
@@ -223,4 +251,31 @@ if __name__ == "__main__":
     estimate_lla_epoch2[:2] *= 180 / np.pi
     print(f"Estimated LLA at epoch 1: {estimate_lla_epoch1}")
     print(f"Estimated LLA at epoch 2: {estimate_lla_epoch2}")
+    print("LS done...")
 
+    # calculate residual vector
+    residual_vec = A @ y - h_A(estimate_result, x_s1=x_s1, x_s2=x_s2)[0]
+    # calculate residual weighted norm, i.e. test statistic z
+    z = residual_vec.T @ np.linalg.inv(A @ R @ A.T) @ residual_vec
+
+    # fault detection
+    # define the threshold T, by the probability of false alarm
+    n = x0.shape[0]  # number of states
+    m = residual_vec.shape[0]  # number of measurements
+    P_fa_set = 0.1  # desired probability of false alarm
+    T = stats.chi2.ppf(1 - P_fa_set, m - n)  # threshold
+
+    # compare z with threshold
+    if z > T:
+        fault_pred = 1  # we predict there is fault, i.e. cycle slip
+    else:
+        fault_pred = 0  # we predict there is no fault, i.e. no cycle slip
+    print(f"Cycle slip detected? => {bool(fault_pred)}")
+
+    # plot z and T on the chi-squared pdf
+    fig, ax = plt.subplots()
+    plot_non_central_chi2(ax, m - n, 0, xlim=60)
+    draw_vertical_line(ax, T, "red", "T")
+    draw_vertical_line(ax, z, "blue", "z")
+    ax.legend()
+    plt.show()
